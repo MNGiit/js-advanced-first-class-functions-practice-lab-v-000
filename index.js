ff780e8526926375cb907b74238bf64615c304c2
Mohammed Noori <kmanoorib@gmail.com>
2019-02-25 23:18:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Code your solution in this file!
+// var array1 = ['a', 'b', 'c'];
+//
+// array1.forEach(function(element) {
+//  console.log(element);
+// });
+
+const logDriverNames = function(drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers, location) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name);
+    }
+  });
+};
+
+
+// var months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.sort();
+// console.log(months);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+// 
+// var array1 = [1, 30, 4, 21, 100000];
+// array1.sort();
+// console.log(array1);
+// expected output: Array [1, 100000, 21, 30, 4]
+
+// var numbers = [4, 2, 5, 1, 3];
+// numbers.sort(function(a, b) {
+//   return a - b;
+// });
+// console.log(numbers);
+
+const driversByRevenue = function(drivers) {
+  // const x = drivers.sort(function(a,b) {return a.revenue-b.revenue});
+  // return x; // needs to return a new array, which the next line will do
+  return drivers.slice().sort(function(a,b) {return a.revenue - b.revenue});
+};
+
+// guestList.sort(function (a, b) {
+//  return a.localeCompare(b);
+// });
+
+const driversByName = function(drivers) {
+  return drivers.slice().sort(function(a,b) {return a.name.localeCompare(b.name)});
+};
+
+const totalRevenue = function(drivers) {
+
+
+uses the reduce() method to sum the revenue of every driver and return the total ‣
+ReferenceError: totalRevenue is not defined
+    at Context.<anonymous> (test/indexTest.js:84:7)expect(totalRevenue(drivers)).to.equal(18500);
